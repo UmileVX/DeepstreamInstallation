f671e8337d4eab79d387b6257189e868f942214b
Yeonwoo Sung <neos960518@gmail.com>
2022-04-15 11:06:10 +0900

    Create install_pip_and_node.sh

---
+++ install_pip_and_node.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+# install nodejs
+sudo apt-get install nodejs
+
+# install pip3, and use pip3 to install pyserial for GPS gadget
+sudo apt-get install python3-pip
+pip3 install pyserial
